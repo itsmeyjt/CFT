72c8b76370bb57feb679fcd5089f1123b8826b13
Juntao You <63189881+itsmeyjt@users.noreply.github.com>
2024-12-27 14:52:55 +0800

    Update infer.sh

--- infer.sh
+++ infer.sh
@@ -2,7 +2,8 @@ base_model=YOUR_MODEL_PATH
 path=YOUR_EVAL_PATH
 result_path=YOUR_RESULT_PATH
 embed_path=YOUR_EMBEDDING_PATH
-cp YOUR_TOKENIZER_PATH/*token*.json ${base_model}/
+info_path=YOUR_INFO_PATH
+# cp YOUR_TOKENIZER_PATH/*token*.json ${base_model}/
 CUDA_VISIBLE_DEVICES=6 python fft_infer.py \
     --base_model ${base_model} \
     --test_data_path YOUR_TEST_JSON_PATH \
@@ -12,9 +13,11 @@ CUDA_VISIBLE_DEVICES=6 python fft_infer.py \
 CUDA_VISIBLE_DEVICES=6 python embedding.py \
     --base_model ${base_model} \
     --embed_path ${embed_path} \
+    --info_path ${info_path} \ 
 
 CUDA_VISIBLE_DEVICES=6 python eval_acc_2.py \
     --path ${path} \
     --result_path ${result_path} \
     --base_model ${base_model} \
     --embed_path ${embed_path} \ 
+    --info_path ${info_path} \ 
